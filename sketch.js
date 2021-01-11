5f25f271dbd7cc750af747c00a6b24d12efe5101
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2021-01-10 21:04:22 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,9 +12,21 @@ function draw() {
   sec = second();  
   angleMode(DEGREES)
   secAngle = map(sec,0,60,0,360)
+  // minAngle = map(sec,0,60*60,0,360)
+  // hrAngle = map(hr,0,60,0,1)
+  pop()
+  translate(200,200)
   rotate(secAngle)
   stroke(255,0,0)
   strokeWeight(7)
   line(0,0,100,0)
-  translate(200,200)
+  noFill()
+  arc(0,0,300,300,0,secAngle);
+  // rotate(minAngle)
+  // stroke(0,250,0)
+  // line(0,0,75,0)
+  // rotate(hrAngle)
+  // stroke(0,0,255)
+  // line(0,0,50,0)
+  pop()
 }
