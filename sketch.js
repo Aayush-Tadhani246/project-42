7f49ad9011eca3b79dea175d1ccd60228fbcc304
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2021-01-11 19:59:56 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,27 +6,50 @@ function setup() {
 
 function draw() {
   background(0,0,0);
+  translate(200,200)
   rotate(-90)
   hr = hour();
   min = minute();
   sec = second();  
   angleMode(DEGREES)
   secAngle = map(sec,0,60,0,360)
-  // minAngle = map(sec,0,60*60,0,360)
-  // hrAngle = map(hr,0,60,0,1)
-  pop()
-  translate(200,200)
+  minAngle = map(min,0,60,0,360)
+  hrAngle = map(hr,0,60,0,360)
+  push()
   rotate(secAngle)
   stroke(255,0,0)
   strokeWeight(7)
   line(0,0,100,0)
-  noFill()
-  arc(0,0,300,300,0,secAngle);
-  // rotate(minAngle)
-  // stroke(0,250,0)
-  // line(0,0,75,0)
-  // rotate(hrAngle)
-  // stroke(0,0,255)
-  // line(0,0,50,0)
   pop()
+  noFill()
+  strokeWeight(10)
+  stroke(255,0,0)
+  arc(0,0,300,300,0,secAngle);
+  
+  
+  push()
+  rotate(minAngle)
+  stroke(0,255,0)
+  strokeWeight(7)
+  line(0,0,75,0)
+  
+  pop()
+  noFill()
+  strokeWeight(10)
+  stroke(0,255,0)
+  arc(0,0,280,280,0,minAngle);
+
+  push()
+  rotate(hrAngle)
+  stroke(0,0,255)
+  strokeWeight(7)
+  line(0,0,50,0)
+  
+  pop()
+  noFill()
+  strokeWeight(10)
+  stroke(0,0,255)
+  arc(0,0,260,260,0,hrAngle);
+  
+ 
 }
